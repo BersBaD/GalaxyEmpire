7083e9dfa6248c6591f2a44c3d6c1a2e15a38b7d
Bers <bers@elite-corp.ru>
2013-10-27 14:52:35 +0400

    Поехали!

---
+++ alliance.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta id="viewport" name="viewport" content="width=320; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+    <title>Galaxy Empire - база данных сервера (ALL35)</title>
+    <link rel="stylesheet" type="text/css" href="css/table.css" />
+  </head>
+
+  <body>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
+      <table class='mystyle' bgcolor='#ffcc00' align='center'>
+        <tr>
+          <th>Поиск по названию альянса</th>
+        </tr>
+        <tr>
+          <th>Альянс</th>
+        </tr>
+        <tr>
+          <td align='center'><input type="text" name="falliance" /></td>
+        </tr>
+        <tr>
+          <td align='center'><input type="submit" name="submit" value="Поиск" /></td>
+        </tr>
+        <tr>
+          <td align='center'><input type=button onClick="location.href='index.php'" value='На главную'></td>
+        </tr>
+      </table>
+    </form>
+  </body>
+
+</html>
+
+<?php
+  if (isset($_POST['submit'])) {
+    $falliance = $_POST['falliance'];
+
+    $db = new SQLite3('Galaxy_Empire_DB.sqlite') or
+    die("failed to open/create the database");
+
+    $results = $db->query("
+      SELECT
+        coordinates.galaxy AS 'Галактика',
+        coordinates.system AS 'Система',
+        coordinates.planet AS 'Планета',
+        planet.player AS 'Игрок',
+        planet.alliance AS 'Альянс',
+        planet.size AS 'Размер',
+        planet.temperature AS 'Температура',
+        planet.moon AS 'Луна',
+        planet.slot1 AS 'Слот №1',
+        planet.slot2 AS 'Слот №2',
+        planet.slot3 AS 'Слот №3'
+      FROM
+        planet
+      INNER JOIN coordinates ON planet.ID = coordinates.ID
+      WHERE
+        planet.alliance = '$falliance'
+      LIMIT 25");
+
+    while ($row = $results->fetchArray()) {
+?>
+      <br/>
+      <table class='mystyle' bgcolor='#ffcc00' align='center'>
+        <tr>
+          <th colspan='2'>Результат</th>
+        </tr>
+        <tr>
+          <td align='center'>Координаты</td>
+          <td align='center'>[<?php echo $row['Галактика'];?>:<?php echo $row['Система'];?>:<?php echo $row['Планета'];?>]</td>
+        </tr>
+        <tr>
+          <td align='center'>Игрок</td>
+          <td align='center'><?php echo $row['Игрок'];?></td>
+        </tr>
+        <tr>
+          <td align='center'>Альянс</td>
+          <td align='center'><?php echo $row['Альянс'];?></td>
+        </tr>
+        <tr>
+          <td align='center'>Размер</td>
+          <td align='center'><?php echo $row['Размер'];?></td>
+        </tr>
+        <tr>
+          <td align='center'>Температура</td>
+          <td align='center'><?php echo $row['Температура'];?></td>
+        </tr>
+        <tr>
+          <td align='center'>Луна</td>
+          <td align='center'><?php echo $row['Луна'];?></td>
+        </tr>
+        <tr>
+          <td align='center'>Слот №1</td>
+          <td align='center'><?php echo $row['Слот №1'];?></td>
+        </tr>
+        <tr>
+          <td align='center'>Слот №2</td>
+          <td align='center'><?php echo $row['Слот №2'];?></td>
+        </tr>
+        <tr>
+          <td align='center'>Слот №3</td>
+          <td align='center'><?php echo $row['Слот №3'];?></td>
+        </tr>
+        <tr>
+          <td align='center' colspan='2'>
+          <?php
+            $file = './images/'.$row['Галактика'].'/'.$row['Система'].'/'.$row['Планета'].'.PNG';
+
+            if (file_exists($file)) {
+                echo "<img src='$file' alt='Что то там' width='100%'>";
+            } else {
+                echo "Нет картинки :(";
+            }
+          ?>
+          </td>
+        </tr>
+      </table>
+      <br/>
+<?php
+    }
+  }
+?>
---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta id="viewport" name="viewport" content="width=320; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+    <title>Galaxy Empire - база данных сервера (ALL35)</title>
+    <link rel="stylesheet" type="text/css" href="css/table.css" />
+  </head>
+
+  <body>
+    <table class='mystyle' bgcolor='#ffcc00' align='center'>
+      <tr>
+        <th>Поиск в базе данных<br/>- Galaxy Empire -<br/>(ALL35)</th>
+      </tr>
+      <tr>
+        <td align='center'>
+          <form action="coordinates.php">
+            <input type="submit" value="Поиск по координатам">
+          </form>
+        </td>
+      </tr>
+      <tr>
+        <td align='center'>
+          <form action="player.php">
+            <input type="submit" value="Поиск по имени игрока">
+          </form>
+        </td>
+      </tr>
+      <tr>
+        <td align='center'>
+          <form action="alliance.php">
+            <input type="submit" value="Поиск по названию альянса">
+          </form>
+        </td>
+      </tr>
+    </table>
+  </body>
+
+</html>
