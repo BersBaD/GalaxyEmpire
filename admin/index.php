257df9d975b35e34ccfc55483ed75d50b103f9aa
Bers <bers@elite-corp.ru>
2013-10-27 21:53:45 +0400

    Latest Commit - Ok

--- admin/index.php
+++ admin/index.php
@@ -3,36 +3,31 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 
   <head>
+    <title>Galaxy Empire - база данных сервера (ALL35)</title>
+
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Content-Language" content="ru" />
+
+    <meta name="description" content="Galaxy Empire - база данных сервера (ALL35)" />
+    <meta name="keywords" content="IOS Game, Galaxy Empire, DB" />
+    <meta name="author" content="BersBaD" />
 
     <meta name="viewport" content="user-scalable=no, width=device-width" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
 
     <link rel="apple-touch-startup-image" href="/images/icon/img/startup.png" />
 
-    <link rel="shortcut icon" href="/images/icon/favicon.ico" />
+    <link rel="icon" href="/images/icon/favicon.ico" sizes="16x16 32x32 48x48 64x64" />
 
-    <link rel="apple-touch-icon" sizes="57x57" href="/images/icon/apple-touch-icon-114.png" />
-    <link rel="apple-touch-icon" sizes="72x72" href="/images/icon/apple-touch-icon-144.png" />
-    <link rel="apple-touch-icon" sizes="144x144" href="/images/icon/apple-touch-icon-144.png" />
-
-    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/images/icon/apple-touch-icon-114-precomposed.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/images/icon/apple-touch-icon-144-precomposed.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/images/icon/apple-touch-icon-144-precomposed.png" />
-
-<link rel="apple-touch-icon" href="touch-icon-iphone.png">
-<link rel="apple-touch-icon" sizes="76x76" href="touch-icon-ipad.png">
-<link rel="apple-touch-icon" sizes="120x120" href="touch-icon-iphone-retina.png">
-<link rel="apple-touch-icon" sizes="152x152" href="touch-icon-ipad-retina.png">
-
-<link rel="icon" href="/favicon.ico" sizes="16x16 32x32 48x48 64x64" type="image/vnd.microsoft.icon">
+    <link rel="apple-touch-icon" href="/images/icon/apple-touch-icon-60x60.png" />
+    <link rel="apple-touch-icon" href="/images/icon/apple-touch-icon-76x76.png" sizes="76x76" />
+    <link rel="apple-touch-icon" href="/images/icon/apple-touch-icon-120x120.png" sizes="120x120" />
+    <link rel="apple-touch-icon" href="/images/icon/apple-touch-icon-152x152.png" sizes="152x152" />
 
     <!-- <link rel=”stylesheet” media=”all and (orientation:portrait)” href=”portrait.css”> -->
     <!-- <link rel=”stylesheet” media=”all and (orientation:landscape)” href=”landscape.css”>  -->
-    <link rel="stylesheet" type="text/css" href="/css/table.css" />
-
-    <title>Galaxy Empire - база данных сервера (ALL35)</title>
+    <link rel="stylesheet" href="/css/table.css" type="text/css" />
   </head>
 
   <body>
--- index.php
+++ index.php
@@ -14,23 +14,20 @@
 
     <meta name="viewport" content="user-scalable=no, width=device-width" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
 
     <link rel="apple-touch-startup-image" href="/images/icon/img/startup.png" />
 
-    <link rel="shortcut icon" href="/images/icon/favicon.ico" />
+    <link rel="icon" href="/images/icon/favicon.ico" sizes="16x16 32x32 48x48 64x64" />
 
-    <link rel="apple-touch-icon" sizes="57x57" href="/images/icon/apple-touch-icon-114.png" />
-    <link rel="apple-touch-icon" sizes="72x72" href="/images/icon/apple-touch-icon-144.png" />
-    <link rel="apple-touch-icon" sizes="144x144" href="/images/icon/apple-touch-icon-144.png" />
-
-    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/images/icon/apple-touch-icon-114-precomposed.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/images/icon/apple-touch-icon-144-precomposed.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/images/icon/apple-touch-icon-144-precomposed.png" />
+    <link rel="apple-touch-icon" href="/images/icon/apple-touch-icon-60x60.png" />
+    <link rel="apple-touch-icon" href="/images/icon/apple-touch-icon-76x76.png" sizes="76x76" />
+    <link rel="apple-touch-icon" href="/images/icon/apple-touch-icon-120x120.png" sizes="120x120" />
+    <link rel="apple-touch-icon" href="/images/icon/apple-touch-icon-152x152.png" sizes="152x152" />
 
     <!-- <link rel=”stylesheet” media=”all and (orientation:portrait)” href=”portrait.css”> -->
     <!-- <link rel=”stylesheet” media=”all and (orientation:landscape)” href=”landscape.css”>  -->
-    <link rel="stylesheet" type="text/css" href="/css/table.css" />
+    <link rel="stylesheet" href="/css/table.css" type="text/css" />
   </head>
 
   <body>
