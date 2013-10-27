b1ce5f731db00795031284b7dffe81d00fd6cacc
Bers <bers@elite-corp.ru>
2013-10-27 18:24:55 +0400

    Latest Commit - Ok

--- alliance.php
+++ alliance.php
@@ -128,7 +128,7 @@
         <tr>
           <td align='center' colspan='2'>
           <?php
-            $pngfile = $webroot.'/images/'.$row['Галактика'].'/'.$row['Система'].'/'.$row['Планета'].'.PNG';
+            $pngfile = '/images/'.$row['Галактика'].'/'.$row['Система'].'/'.$row['Планета'].'.PNG';
 
             if (file_exists($pngfile)) {
                 echo "<img src='$pngfile' alt='Что то там' width='100%'>";
